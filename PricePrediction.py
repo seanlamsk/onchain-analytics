95ec716861a73e008c840a811b6e80c1fe93cd79
casperlim2018 <casper.lim.2018@sis.smu.edu.sg>
2021-11-05 19:52:30 +0800

    Updated dashboard v2

--- PricePrediction.py
+++ PricePrediction.py
@@ -34,14 +34,26 @@ class PricePrediction:
         arima_pred_card =  create_card(
             [
                 html.H4('ARIMA Price Prediction'),
-                html.Div(id='Arima_pred'),
+                dbc.Row([
+                    dbc.Col(html.Div(id='Arima_pred')),
+                    dbc.Col(html.Div(id='Arima_change', style={
+                    "color": 'Black'
+                })),
+                ]
+                )
             ]
         )
 
         lstm_pred_card =  create_card(
             [
                 html.H4('LSTM Price Prediction'),
-                html.Div(id='Lstm_pred'),
+                dbc.Row([
+                dbc.Col(html.Div(id='Lstm_pred')),
+                dbc.Col(html.Div(id='Lstm_change', style={
+                    "color": 'Black'
+                })),
+                ]
+                )
             ]
         )
 
@@ -61,6 +73,7 @@ class PricePrediction:
 
         slider_card =  create_card(
             [
+                html.H6('Date of Prediction:'),
                 Slider(id='date-selector',
                     min=0, 
                     max=9,
@@ -94,30 +107,31 @@ class PricePrediction:
         )
 
         self.layout = html.Div([
-            html.H1('Price Prediction Models'),
+            html.H3('Price Prediction Models'),
             html.Br(),
             html.Div([
                 dbc.Row([
-                    dbc.Col([dropdown_card])
-                ]), 
-                dbc.Row([
-                    dbc.Col([slider_card])
+                    dbc.Col([
+                        Graph(
+                            id = 'comparison-graph'
+                        ),]
+                    ),
+                    dbc.Col([
+                        dbc.Row([
+                            dbc.Col([dropdown_card])
+                        ]), 
+                        dbc.Row([
+                            dbc.Col([slider_card])
+                        ]),
+                        dbc.Row([
+                            dbc.Col([arima_pred_card]), dbc.Col([lstm_pred_card])
+                        ]),
+                        dbc.Row([
+                            dbc.Col([returns_card]), dbc.Col([volatility_card])
+                        ]),]
+                    )
                 ]),
-                dbc.Row([
-                    dbc.Col([arima_pred_card]), dbc.Col([lstm_pred_card])
-                ]),
-                dbc.Row([
-                    dbc.Col([returns_card]), dbc.Col([volatility_card])
-                ]),]),
-            html.H4('Price Prediction Graph'),
-            Graph(
-                id = 'comparison-graph'
-            ),
-            html.Div(id='test'),
-            html.Br(),
-            # dcc.Link('Go to Page 2', href='/page-2'),
-            html.Br(),
-            dcc.Link('Go back to home', href='/'),
+            ]),
         ])
 
         @self.app.callback(dash.dependencies.Output('comparison-graph', 'figure'),
@@ -130,9 +144,9 @@ class PricePrediction:
                 self.coin = 'ltc'
             self.prediction = pd.read_csv(f'models/predictions/{self.coin}_price_pred_prediction.csv')
             self.actual = pd.read_csv(f'models/predictions/{self.coin}_price_pred_actual.csv')
-            traceArima = go.Scatter(x=self.prediction['Date'], y = self.prediction[f'ARIMA'], mode = 'lines', name = "ARIMA")
-            traceLstm = go.Scatter(x=self.prediction['Date'], y = self.prediction[f'LSTM'], mode = 'lines', name = "LSTM")
-            traceActual = go.Scatter(x=self.actual["Date"], y = self.actual[f'close'], mode = 'lines', name = "Actual Price")
+            traceArima = go.Scatter(x=self.prediction['Date'], y = self.prediction['ARIMA'], mode = 'lines', name = "ARIMA")
+            traceLstm = go.Scatter(x=self.prediction['Date'], y = self.prediction['LSTM'], mode = 'lines', name = "LSTM")
+            traceActual = go.Scatter(x=self.actual["Date"], y = self.actual['close'], mode = 'lines', name = "Actual Price")
             df = [traceArima, traceLstm, traceActual]
             layout = go.Layout(title = 'Price Prediction of LSTM and ARIMA with ' + value)
             figure = go.Figure(data=df, layout=layout)
@@ -146,7 +160,7 @@ class PricePrediction:
                 coin = 'eth'
             elif value == 'Litecoin':
                 coin = 'ltc'
-            return self.stats.loc[coin, 'Annualised Returns']
+            return f"{self.stats.loc[coin, 'Annualised Returns']:.2f}%"
 
         @self.app.callback(dash.dependencies.Output('volatility', 'children'),
                     [dash.dependencies.Input('crypto-select', 'value')])
@@ -156,24 +170,51 @@ class PricePrediction:
                 coin = 'eth'
             elif value == 'Litecoin':
                 coin = 'ltc'
-            return self.stats.loc[coin, 'Volatility']
-        # @self.app.callback(dash.dependencies.Output('test', 'children'),
-        #             [dash.dependencies.Input('date-selector', 'value')])
-        # def up(value):
-        #     return value
+            return f"{self.stats.loc[coin, 'Volatility']:.2f}"
 
-        @self.app.callback(dash.dependencies.Output('Arima_pred', 'children'), dash.dependencies.Output('date-selector', 'marks'),
+        @self.app.callback(dash.dependencies.Output('Arima_pred', 'children'), 
+                    dash.dependencies.Output('date-selector', 'marks'), 
+                    dash.dependencies.Output('Arima_change', 'children'), 
+                    dash.dependencies.Output('Arima_change', 'style'),
                     dash.dependencies.Input('date-selector', 'value'),
                     dash.dependencies.Input('crypto-select', 'value')
                     )
         def update_pred_arima(value1, value2):
             self.value = value1+1
-            return (self.prediction.iloc[self.value]['ARIMA'], {i-1: '{}'.format(self.prediction['Date'][i]) for i in range(1, len(self.prediction['Date']))})
+            pred_value = self.prediction.iloc[self.value]['ARIMA']
+            change = pred_value - self.actual.iloc[-1]['close'] 
+            percent_change = change/self.actual.iloc[-1]['close']*100
+            if percent_change > 0:
+                colorarima = 'Green'
+                symbol = '+'
+            elif percent_change < 0:
+                colorarima = 'Red'
+                symbol = ''
+            else:
+                colorarima = 'Black' 
+                symbol = ''
+            return_value = f'{symbol}{change:,.2f} ({symbol}{percent_change:,.2f}%)'
+            return (f"US$ {pred_value:,.2f}", {i-1: '{}'.format(self.prediction['Date'][i]) for i in range(1, len(self.prediction['Date']))}, return_value, {"color": colorarima})
             
-        @self.app.callback(dash.dependencies.Output('Lstm_pred', 'children'),
+        @self.app.callback(dash.dependencies.Output('Lstm_pred', 'children'), 
+                    dash.dependencies.Output('Lstm_change', 'children'), 
+                    dash.dependencies.Output('Lstm_change', 'style'),
                     dash.dependencies.Input('date-selector', 'value'),
                     dash.dependencies.Input('crypto-select', 'value')
                     )
         def update_pred_lstm(value1, value2):
             self.value = value1+1
-            return self.prediction.iloc[self.value]['LSTM']
+            pred_value = self.prediction.iloc[self.value]['LSTM']
+            change = pred_value - self.actual.iloc[-1]['close'] 
+            percent_change = change/self.actual.iloc[-1]['close']*100
+            if percent_change > 0:
+                colorlstm = 'Green'
+                symbol = '+'
+            elif percent_change < 0:
+                colorlstm = 'Red'
+                symbol = ''
+            else:
+                colorlstm = 'Black' 
+                symbol = ''
+            return_value = f'{symbol}{change:,.2f} ({symbol}{percent_change:,.2f}%)'
+            return (f"US$ {pred_value:,.2f}", return_value, {"color": colorlstm})
