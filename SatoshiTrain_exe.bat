52eb2240e44615726f3f1cc77344107d99cdad2a
casperlim2018 <casper.lim.2018@sis.smu.edu.sg>
2021-11-07 13:47:18 +0800

    Added bat files to run the code
    bat files for importing libraries, train model and run dash gui

---
+++ SatoshiTrain_exe.bat
@@ -0,0 +1,2 @@
+@python.exe train.py %*
+@pause
---
+++ package_exe.bat
@@ -0,0 +1,11 @@
+pip install --user numpy %*
+pip install --user pandas %*
+pip install --user dash %*
+pip install --user functools %*
+pip install --user scipy %*
+pip install --user seaborn %*
+pip install --user matplotlib %*
+pip install --user tensorflow %*
+pip install --user sklearn %*
+pip install --user keras %*
+@pause
